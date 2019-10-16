a09963862c32d7f80d41db11c0c3db0332e12a2b
Zhao Qi <qi.zhao1991@gmail.com>
2019-10-16 16:32:23 +0200

    Update tfrecords_test.py

--- utils/tfrecords_test.py
+++ utils/tfrecords_test.py
@@ -14,12 +14,33 @@ flags.DEFINE_integer('num_classes', 13, 'The number of label classes')
 
 FLAGS = flags.FLAGS
 
-if __name__ == '__main__':
-    mode = 'tf
-    batch_size = FLAGS.batch_size
-    num_classes = FLAGS.num_classes
-    tfrecord_file = FLAGS.tfrecord_file
-    
+def test_tfrecords(tfrecords_file):
+    """
+    """
+    tf_images, tf_labels = read_tfrecords(tfrecords_file)
+    # tf_images = preprocessing_image(tf_images, mode)
+    # images_batch, labels_batch = generate_image_label_batch(tf_images, tf_labels, batch_size=batch_size, num_classes=num_classes)
+    # open a session 
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        for i in range(4):
+            images, labels = sess.run([tf_images, tf_labels])
+            print('image shape: {}, image tpye: {}, label type: {}'.format(images.shape, images.dtype, labels.dtype))
+        
+        coord.request_stop()
+        coord.join(threads)
+
+        show_image(images.astype(np.uint8), 'image')
+
+
+def test_tfrecords_with_batch(tfrecord_file, batch_size, num_classes, mode='tf'):
+    """test function
+    """
+    # batch_size = FLAGS.batch_size
+    # num_classes = FLAGS.num_classes
     tf_images, tf_labels = read_tfrecords(tfrecord_file)
     tf_images = preprocessing_image(tf_images, mode)
     images_batch, labels_batch = generate_image_label_batch(tf_images, tf_labels, batch_size=batch_size, num_classes=num_classes)
@@ -36,7 +57,20 @@ if __name__ == '__main__':
         coord.request_stop()
         coord.join(threads)
 
-        # show_image((images[0, :, :, :]), 'image')
+        show_image((images[0, :, :, :]), 'image')
         # print(images[0, :, :, :], labels[0])
+# test_tfrecords_with_batch('./17_flowers/tfrecords/valid_tfrecords.tfrecords')
 
+if __name__ == '__main__':
+    mode = 'tf
+    batch_size = FLAGS.batch_size
+    num_classes = FLAGS.num_classes
+    tfrecord_file = FLAGS.tfrecord_file
+    print('Test read_tfrecords and visualize image without using batch size')
+    test_tfrecords(tfrecords_file)
+    
+    print('Test read_tfrecords and visualize image using batch size')
+    test_tfrecords_with_batch(tfrecords_file, batch_size, num_classes, mode)
+    
+    
     
